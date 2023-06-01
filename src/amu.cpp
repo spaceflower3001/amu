1da7a36d21adb4d951e197b9a425b3aa75a772a8
pack3001 <mahd0b9@gmail.com>
2023-06-01 14:10:05 +0000

    Fix & display amu logo
    I fixed and was able to display the amu logo!

--- src/amu.cpp
+++ src/amu.cpp
@@ -1,14 +1,19 @@
 // spaceflower3001
 
 #include <ncurses.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 const char *menu_logo[4] = 
 {
- "	 __ _  _ __ ___   _   _",
- "	/ _` || '_ ` _ \ | | | |",
- "     | (_| || | | | | || |_| |",
- "	\__,_||_| |_| |_| \__,_|"
+ "  __ _  _ __ ___   _   _ ",
+ " / _` || '_ ` _ | | | | |",
+ "| (_| || | | | | || |_| |",
+ " \\__,_||_| |_| |_| \\__,_|"
 };
+
+
 
 int main() 
 {
@@ -17,17 +22,12 @@ int main()
     cbreak(); // disable line buffering
     curs_set(0); // hide cursor
 
-    clear();
+    int y = (LINES - 4) / 2;
+    int x = (COLS - 25) / 2;
 
-    int height = LINES * 0.75; // calculate box height
-    int width = height * 4 / 3; // calculate box width
-    int y = (LINES - height) / 2; // calculate box y position
-    int x = (COLS - width) / 2; // calculate box x position
-
-    WINDOW *win = newwin(height, width, y, x); // create window
-    box(win, 0, 0); // draw box around window
-    wrefresh(win); // refresh window
-
+    for (int i = 0; i < 4; i++) {
+        mvprintw(y + i, x, menu_logo[i]);
+    }
     getch(); // wait for user input
 
     endwin(); // end ncurses
