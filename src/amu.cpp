4110abdfc583198d8d85eaec510c69b7a51bc3ec
pack3001 <mahd0b9@gmail.com>
2023-06-02 15:55:18 +0000

    Add .gitignore, tab code

--- src/amu.cpp
+++ src/amu.cpp
@@ -1,43 +1,43 @@
 // spaceflower3001
 
+#include <iostream>
 #include <ncurses.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include <string>
+#include <locale>
+#include <cwchar>
 
 const char *menu_logo[4] = 
 {
- "  __ _  _ __ ___   _   _ ",
- " / _` || '_ ` _ | | | | |",
- "| (_| || | | | | || |_| |",
- " \\__,_||_| |_| |_| \\__,_|"
+	"  __ _  _ __ ___   _   _ ",
+	" / _` || '_ ` _ | | | | |",
+	"| (_| || | | | | || |_| |",
+	" \\__,_||_| |_| |_| \\__,_|"
 };
 
 void draw_logo()
 {
-  int y = (LINES - 4) / 2;
-  int x = (COLS - 25) / 2;
+	int y = (LINES - 4) / 2;
+	int x = (COLS - 25) / 2;
 
-  for (int i = 0; i < 4; i++) 
-  {
-	  mvprintw(y + i, x, menu_logo[i]);
-  }
+	for (int i = 0; i < 4; i++) 
+	{
+	mvprintw(y + i, x, menu_logo[i]);
+	}
 }
 
 int main() 
 {
-    initscr(); // initialize ncurses
-    noecho(); // don't echo user input
-    cbreak(); // disable line buffering
-    curs_set(0); // hide cursor
+	initscr(); // initialize ncurses
+	noecho(); // don't echo user input
+	cbreak(); // disable line buffering
+	curs_set(0); // hide cursor
 
-    draw_logo();
+	refresh();
 
-    getch(); // wait for user input
+	draw_logo();
 
-    endwin(); // end ncurses
-    return 0;
+	getch(); // wait for user input
+
+	endwin(); // end ncurses
+	return 0;
 }
-
-
-
